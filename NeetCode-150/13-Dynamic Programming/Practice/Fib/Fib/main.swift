36a961d9f867d0d3029e5cdb3e609d23aee9d6a1
Shawn Frank <shawn@shawn.design>
2025-05-05 06:41:05 +1000

    fib dynamic programming

---
+++ NeetCode-150/13-Dynamic Programming/Practice/Fib/Fib/main.swift
@@ -0,0 +1,33 @@
+//
+//  main.swift
+//  Fib
+//
+//  Created by Shawn Frank on 5/5/2025.
+//
+
+import Foundation
+
+func fib(of value: Int) -> Int {
+    
+    var fibMemo = [Int: Int]()
+    
+    func fibRecursive(of value: Int) -> Int {
+        guard fibMemo[value] == nil else { return fibMemo[value, default: 0] }
+        
+        guard value != 0 else { return 0 }
+        
+        guard value > 2 else { return 1 }
+        
+        let result = fibRecursive(of: value - 1) + fibRecursive(of: value - 2)
+        fibMemo[value] = result
+        return result
+    }
+    
+    return fibRecursive(of: value)
+}
+
+
+print(fib(of: 2))
+print(fib(of: 5))
+print(fib(of: 15))
+print(fib(of: 40))
