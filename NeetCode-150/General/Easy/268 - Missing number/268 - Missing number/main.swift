c8b5d9591d3e3e6b0c9953fb153e5828565b19a7
Shawn Frank <shawn@shawn.design>
2025-06-04 06:17:15 +1000

    268 missing number

---
+++ NeetCode-150/General/Easy/268 - Missing number/268 - Missing number/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  268 - Missing number
+//
+//  Created by Shawn Frank on 4/6/2025.
+//
+//  https://leetcode.com/problems/missing-number/
+
+import Foundation
+
+func missingNumber(_ nums: [Int]) -> Int {
+    var expectedSum = 0
+    
+    for num in 0 ... nums.count {
+        expectedSum += num
+    }
+    
+    var actualSum = 0
+    
+    for num in nums {
+        actualSum += num
+    }
+    
+    return expectedSum - actualSum
+}
+
+print(missingNumber([3,0,1]))
+print(missingNumber([0,1]))
+print(missingNumber([9,6,4,2,3,5,7,0,1]))
